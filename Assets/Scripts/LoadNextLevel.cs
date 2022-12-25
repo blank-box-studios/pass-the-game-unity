478bdb27e33c4d3606254fcb5f4b179dd142797b
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2022-12-24 20:39:08 -0800

    failed attempt at changing levels

---
+++ Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+
+    private void onTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triggered");
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}

b9a7fd15f972b51dc8d94db46b74696422b7243a
Adwit Rahman <adwitrahman@gmail.com>
2022-12-25 12:33:37 -0500

    Couple of changes
    -Added TextMeshPro
    -Added font Space Grotesk
    -Added canvas fonts
    -Clamped and rounded force added to player on each drag (slingshot)

--- Assets/Scripts/PlayerInteractionScript.cs
+++ Assets/Scripts/PlayerInteractionScript.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerInteractionScript : MonoBehaviour
 {
+
+    //To be removed.
+    public TextMeshProUGUI testDragText;
+    
+
+
+
     [SerializeField] Rigidbody2D playerRb;
     [SerializeField] float dragDistance;
     [SerializeField] bool isDragging;
@@ -27,8 +35,13 @@ public class PlayerInteractionScript : MonoBehaviour
 
     void addForce(float draggedDistance)
     {
+
+        float forceToAdd = Mathf.Clamp( Mathf.RoundToInt(draggedDistance), 200, 1000);
+
         Vector3 dir = Quaternion.AngleAxis(angleForForce, Vector3.forward) * Vector3.right;
-        playerRb.AddForce(-dir * draggedDistance);
+        testDragText.text = forceToAdd.ToString();
+        playerRb.AddForce(-dir * forceToAdd);
+
     }
 
     void rotateIndicator()
@@ -45,7 +58,7 @@ public class PlayerInteractionScript : MonoBehaviour
 
         pullIndicatorObject.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90));
 
-        gfx.localScale = new Vector3(0.1f, dragDistance / 100.0f, 0);
+        gfx.localScale = new Vector3(1f, dragDistance / 1000.0f, 0);
     }
 
     void calculateDragDistance()
